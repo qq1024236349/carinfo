1b46eec16e1670a5f2ace70defa2676ab9f104a5
xionggang <xionggang@126.com>
2017-08-07 22:28:25 +0800

    package

---
+++ src/Ford/Focus/Focus2016.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Author: xionggang  <1024236349@qq.com>
+ * Date: 17/8/7
+ * Time: 下午10:25
+ */
+namespace Ford\Focus;
+
+class Focus2016
+{
+	public function info()
+	{
+		echo "This is Ford Focus2016!<br>";
+	}
+}
